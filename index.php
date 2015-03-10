f5462076b94a3cfebe34e86010a7dd7a49bdd0d7
ershov-ilya <ershov.ilya@gmail.com>
2015-03-10 09:36:05 +0300

    config files

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author:   ershov-ilya
+ * GitHub:   https://github.com/ershov-ilya/
+ * About me: http://about.me/ershov.ilya (EN)
+ * Website:  http://ershov.pw/ (RU)
+ * Date: 06.03.2015
+ * Time: 15:30
+ */
+
+header('Content-Type: text/plain; charset=utf-8');
+error_reporting(E_ERROR | E_WARNING);
+ini_set("display_errors", 1);
+
+print "OK";
