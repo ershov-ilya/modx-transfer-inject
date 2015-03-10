12908dae1db2e9923a8994153e38bda1e58235e3
ershov-ilya <ershov.ilya@gmail.com>
2015-03-10 15:50:02 +0300

    class Transfer

---
+++ core/class/transfer/transfer.class.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author:   ershov-ilya
+ * GitHub:   https://github.com/ershov-ilya/
+ * About me: http://about.me/ershov.ilya (EN)
+ * Website:  http://ershov.pw/ (RU)
+ * Date: 10.03.2015
+ * Time: 16:13
+ */
+
+class Transfer {
+    private $dbc;
+    private $table_name;
+
+    function __construct($DBC, $table_name='import_map'){
+        /* @var Database $DBC */
+        $this->dbc=$DBC;
+        $this->table_name=$table_name;
+    }
+
+    public static function test(){
+        return "Transfer class: OK\n";
+    }
+
+    public function ptr($entity, $oldID){
+        $sql="SELECT * FROM $this->table_name WHERE entity='$entity' AND donor_id='$oldID'";
+        $res=$this->dbc->get($sql);
+        return $res['aceptor_id'];
+    }
+
+    public function is_exist($entity, $oldID){
+        $sql="SELECT * FROM $this->table_name WHERE entity='$entity' AND donor_id='$oldID'";
+        $res=$this->dbc->get($sql);
+        if(empty($res)) return false;
+        return true;
+    }
+
+    public function not($entity, $oldID){
+        $sql="SELECT * FROM $this->table_name WHERE entity='$entity' AND donor_id='$oldID'";
+        $res=$this->dbc->get($sql);
+        if(empty($res)) return true;
+        return false;
+    }
+
+    public function log($map_link){
+        $res=$this->dbc->putOne('import_map', $map_link);
+        return $res;
+    }
+}
--- do/transfer/resource/index.php
+++ do/transfer/resource/index.php
@@ -18,10 +18,14 @@ define('WRITE', false);
 
 require_once('../../../core/config/core.config.php');
 require_once(API_CORE_PATH.'/class/database/database.class.php');
-require_once(API_CORE_PATH.'/class/recurse-modx/recurse-modx.class.php');
+require_once(API_CORE_PATH.'/class/transfer/transfer.class.php');
+//require_once(API_CORE_PATH.'/class/recurse-modx/recurse-modx.class.php');
 
 /* @var Database $sbs*/
 $sbs = new Database(API_CONFIG_PATH.'/sbs.pdo.config.php');
+/* @var Transfer $import */
+$transfer = new Transfer($sbs);
+
 /* @var Database $base*/
 $base = new Database(API_CONFIG_PATH.'/base.pdo.config.php');
 
@@ -29,6 +33,11 @@ $tablename='modx_site_content';
 $name_field='pagetitle';
 
 $resources=$base->getTable($tablename);
+
+//print_r($resources[10]);
+var_dump($transfer->is_exist('resource', 0));
+
+exit(0);
 $i=0;
 foreach($resources as $resource){
     $map_link=array( 'entity'=>'resource', 'name'=>$resource[$name_field], 'donor_id' => $resource['id']);
@@ -36,11 +45,14 @@ foreach($resources as $resource){
     unset($resource['id']);
     $res='';
 
+    // Вносим в новый сайт
     if(WRITE) {
         $res=$sbs->putOne($tablename, $resource);
         print "$tablename insert:".$res."\n";
     }
     $map_link['aceptor_id']=$res;
+
+    // Вносим строку в карту
     if(WRITE) {
         $res=$sbs->putOne('import_map', $map_link);
         print "import_map insert:".$res."\n";
