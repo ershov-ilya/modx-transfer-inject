e7afab2d9618872d492fe956365e44301cee2236
ershov-ilya <ershov.ilya@gmail.com>
2015-03-11 11:47:51 +0300

    HTML guide in index.php

--- index.php
+++ index.php
@@ -9,27 +9,85 @@
  * Time: 15:30
  */
 
-header('Content-Type: text/plain; charset=utf-8');
+header('Content-Type: text/html; charset=utf-8');
 error_reporting(E_ERROR | E_WARNING);
 ini_set("display_errors", 1);
-define('DEBUG', true);
+define('DEBUG', false);
+?>
 
-require_once('core/config/core.config.php');
-require_once(API_CORE_PATH.'/class/database/database.class.php');
-require_once(API_CORE_PATH.'/class/recurse-modx/recurse-modx.class.php');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
 
-/* @var Database $sbs*/
-$sbs = new Database(API_CONFIG_PATH.'/sbs.pdo.config.php');
-/* @var Database $base*/
-$base = new Database(API_CONFIG_PATH.'/base.pdo.config.php');
+    <title>Cover Template for Bootstrap</title>
 
-//$res=$sbs->getTable('import_map');
-//print_r($res);
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
 
-$rbase=new RecurseMODX($base);
-print_r($rbase->recurseFrom(0));
 
-$docList=$rbase->listFrom(0);
-//asort($docList);
-print_r($docList);
+    <!-- Custom styles for this template -->
+    <link href="cover.css" rel="stylesheet">
 
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<div class="site-wrapper">
+
+    <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+            <div class="inner cover">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h1 class="cover-heading">Eng</h1>
+                        <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+                        <p class="lead">
+                            <a href="#" class="btn btn-lg btn-default">Learn more</a>
+                        </p>
+                    </div>
+                    <div class="col-sm-6">
+                        <h1 class="cover-heading">Rus</h1>
+                        <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+                        <p class="lead">
+                            <a href="#" class="btn btn-lg btn-default">Learn more</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mastfoot">
+                <div class="inner">
+                    <p>Project <a href="https://github.com/ershov-ilya/modx-transfer-inject" target="_blank">modx-transfer-inject</a>, by <a href="https://github.com/ershov-ilya" target="_blank">ILYA ERSHOV</a>.</p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<!-- Latest compiled and minified CSS -->
+<!-- Latest compiled and minified JavaScript -->
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+</body>
+</html>
