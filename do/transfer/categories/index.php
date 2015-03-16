ad5972cc05261663a9ca9567fc30f3c0c7c039c7
ershov-ilya <ershov.ilya@gmail.com>
2015-03-16 13:41:04 +0300

    index.htm menu item do/categories

--- do/transfer/categories/index.php
+++ do/transfer/categories/index.php
@@ -13,7 +13,7 @@ header('Content-Type: text/html; charset=utf-8');
 error_reporting(E_ERROR | E_WARNING);
 ini_set("display_errors", 1);
 define('DEBUG', true);
-define('WRITE', false);
+define('WRITE', true);
 
 require_once('../../../core/config/core.config.php');
 require_once(API_CORE_PATH.'/class/database/database.class.php');
